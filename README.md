034ae33cc34d2ec7c86d713ae21868e38c45895e
gilberto-maess <gilberto@maess.com.br>
2024-03-09 13:18:53 -0300

    projeto de estudo para o curso de sistemas para internet

---
+++ README.md
@@ -0,0 +1,1 @@
+# programacao-de-solucoes-computacionais-estudo
---
+++ src/main/java/com/anima/App.java
@@ -0,0 +1,44 @@
+package com.anima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        String[] emails = new String[] {
+                "teste@localhost",
+                "   @localhost",
+                "teste@     ",
+                "teste",
+                "@localhost",
+                "teste@",
+                "   @    ",
+                "@"
+        };
+
+        for (String email : emails) {
+            System.out.println(
+                    String.format(
+                            "O email %s é %s.",
+                            email,
+                            validarEmail(email)));
+        }
+    }
+
+    private static boolean validarEmail(String email) {
+        if (isNullOrEmpty(email)) {
+            return false;
+        }
+
+        String[] vetorEmail = email.split("@");
+
+        return vetorEmail.length == 2
+                && !isNullOrEmpty(vetorEmail[0])
+                && !isNullOrEmpty(vetorEmail[1]);
+    }
+
+    private static boolean isNullOrEmpty(String str) {
+        return str == null || str.trim().isEmpty();
+    }
+}
